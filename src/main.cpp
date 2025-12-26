93765840c9bc867e9eef9e97245be0ef1e6a8740
orpon-chanda <orpon137@gmail.com>
2025-12-26 20:46:39 +0600

    init

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+ Serial.begin(9600);
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  Serial.println(x + y);
+  return x + y;
+}
